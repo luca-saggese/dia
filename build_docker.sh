068798725dec177d89748eff80553ee7d75a35fa
Luca <luca.saggese@gmail.com>
2025-06-09 21:40:53 +0200

    wip

--- build_docker.sh
+++ build_docker.sh
@@ -2,4 +2,4 @@
 docker build -t dia .
 echo "✅ Build completata!"
 echo "👉 Per eseguire il container usa:"
-echo "docker run --rm -it --gpus all -p 8083:8080 --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v /data/huggingface:/huggingface -v /home/lvx/output:/app/output dia"
+echo "docker run --rm -it --gpus all -p 8075:7960 --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -v /data/huggingface:/huggingface -v /home/lvx/output:/app/output dia"
